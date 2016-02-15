99caa0ee43da7c2f4f8eedf00ed8c792367c33eb
Dave Nicponski <dave.nicponski@gmail.com>
2016-02-15 18:51:59 -0500

    Change the means of passing branch back

--- templates.go
+++ templates.go
@@ -20,13 +20,13 @@ GitHub Pull Request Rejection Bot</h1>
     Re-enable pull requests ({{.Branch}})
   </button>
 {{else}}
-  <button id="disable" style="position: relative" type="submit" formaction="/disable/{{.Repo.Owner.Login}}/{{.Repo.Name}}#" formmethod="POST">
+  <button id="disable" style="position: relative" type="submit" formaction="/disable/{{.Repo.Owner.Login}}/{{.Repo.Name}}" formmethod="POST">
     Disable (all)
   </button>
-   <button id="disable2" style="position: relative" type="submit" formaction="/disable/{{.Repo.Owner.Login}}/{{.Repo.Name}}#master" formmethod="POST">
+   <button id="disable2" style="position: relative" type="submit" formaction="/disable/{{.Repo.Owner.Login}}/{{.Repo.Name}}____master" formmethod="POST">
     Disable (master)
   </button>
-   <button id="disable3" style="position: relative" type="submit" formaction="/disable/{{.Repo.Owner.Login}}/{{.Repo.Name}}#stage" formmethod="POST">
+   <button id="disable3" style="position: relative" type="submit" formaction="/disable/{{.Repo.Owner.Login}}/{{.Repo.Name}}____stage" formmethod="POST">
     Disable (stage)
   </button>
 
